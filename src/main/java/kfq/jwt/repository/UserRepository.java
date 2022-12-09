432202a94d141ab5361111ea088753af005b1b19
bookpark <bkbkgg@gmail.com>
2022-12-09 11:02:32 +0900

    UserRepository

--- src/main/java/kfq/jwt/repository/UserRepository.java
+++ src/main/java/kfq/jwt/repository/UserRepository.java
@@ -3,5 +3,5 @@ package kfq.jwt.repository;
 import kfq.jwt.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, String> {
 }
