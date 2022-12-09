7b53c46d75be993c9dfe829f0d84ed4528bb70bd
bookpark <bkbkgg@gmail.com>
2022-12-09 11:04:01 +0900

    config 추가

---
+++ src/main/java/kfq/jwt/common/SecurityConfig.java
@@ -0,0 +1,22 @@
+package kfq.jwt.common;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable();
+        http.httpBasic().disable().authorizeRequests()
+                .antMatchers("/login/**").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+}
