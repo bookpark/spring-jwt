778c2c17391753c7cc943cd2cdae1e14438704e6
bookpark <bkbkgg@gmail.com>
2022-12-09 11:03:50 +0900

    Custom userdetails 서비스 추가

---
+++ src/main/java/kfq/jwt/service/CustomUserDetailsService.java
@@ -0,0 +1,19 @@
+package kfq.jwt.service;
+
+import kfq.jwt.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findById(username).orElse(null);
+    }
+}
